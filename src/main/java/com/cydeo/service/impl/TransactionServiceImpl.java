055efdf64c65159ead9d6a19e5303fafd1b4f369
HP Laptop <katharina.slava@gmail.com>
2023-12-02 11:30:25 -0500

    adding-exception-class

--- src/main/java/com/cydeo/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TransactionServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.exception.BadRequestException;
 import com.cydeo.model.Account;
 import com.cydeo.model.Transaction;
 import org.springframework.stereotype.Component;
@@ -13,7 +14,35 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public Transaction makeTransfer(Account sender, Account receiver, BigDecimal amount, Date creationDate, String message) {
+        /*
+               -if sender or receiver is null ?
+               -if sender and receiver is the same account ?
+               -if sender has enough balance to make transfer ?
+               -if both accounts are checking, if not, one of them saving, it needs to be same userId
+         */
+
+
+        validateAccount(sender, receiver);
+
+        //make transfer
+        
         return null;
+    }
+
+    private void validateAccount(Account sender, Account receiver) {
+
+                /*
+            -if any of the account is null
+            -if account ids are the same(same account)
+            -f the account exist in the database (repository)
+         */
+        if(sender==null ||receiver==null){
+            throw new BadRequestException("Sender or Receiver cannot be null");
+        }
+
+
+
+
     }
 
     @Override

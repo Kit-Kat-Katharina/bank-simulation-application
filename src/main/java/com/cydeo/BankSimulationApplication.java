8d21fdb80cdb049161cdfda7f65a62af6c55749a
HP Laptop <katharina.slava@gmail.com>
2023-12-02 14:40:29 -0500

    adding-commented-main-method

--- src/main/java/com/cydeo/BankSimulationApplication.java
+++ src/main/java/com/cydeo/BankSimulationApplication.java
@@ -17,21 +17,21 @@ public class BankSimulationApplication {
     public static void main(String[] args) {
 
         ApplicationContext container = SpringApplication.run(BankSimulationApplication.class, args);
-
-        //get account and transaction service beans
-        AccountService accountService = container.getBean(AccountService.class);
-        TransactionService transactionService = container.getBean(TransactionService.class);
-
-        //create 2 account sender and receiver
-       Account sender =  accountService.createNewAccount(BigDecimal.valueOf(70), new Date(), AccountType.CHECKING, 1L);
-
-       Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50), new Date(), AccountType.CHECKING, 2L);
-       Account receiver2 = null;
-
-       accountService.listAllAccount().forEach(System.out::println);
-       transactionService.makeTransfer(sender,receiver,new BigDecimal(40),new Date(),"Transaction 1");
-        System.out.println(transactionService.findAllTransaction().get(0));
-        accountService.listAllAccount().forEach(System.out::println);
+//
+//        //get account and transaction service beans
+//        AccountService accountService = container.getBean(AccountService.class);
+//        TransactionService transactionService = container.getBean(TransactionService.class);
+//
+//        //create 2 account sender and receiver
+//       Account sender =  accountService.createNewAccount(BigDecimal.valueOf(70), new Date(), AccountType.CHECKING, 1L);
+//
+//       Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50), new Date(), AccountType.CHECKING, 2L);
+//       Account receiver2 = null;
+//
+//       accountService.listAllAccount().forEach(System.out::println);
+//       transactionService.makeTransfer(sender,receiver,new BigDecimal(40),new Date(),"Transaction 1");
+//        System.out.println(transactionService.findAllTransaction().get(0));
+//        accountService.listAllAccount().forEach(System.out::println);
     }
 
 }
